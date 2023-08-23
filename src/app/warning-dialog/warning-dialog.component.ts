72a34ebc42f18f49fb21980a8f72f51950c2eac0
Roman <@>
2023-08-23 16:49:16 +0400

    fix: path to styles

--- src/app/warning-dialog/warning-dialog.component.ts
+++ src/app/warning-dialog/warning-dialog.component.ts
@@ -5,7 +5,7 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 @Component({
   selector: 'app-warning-dialog',
   templateUrl: './warning-dialog.component.html',
-  styleUrls: ['./warning-dialog.component.css'],
+  styleUrls: ['./warning-dialog.component.scss'],
 })
 export class WarningDialogComponent {
   constructor(
