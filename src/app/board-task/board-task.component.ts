41bf02bc4f1be718c7dbd51e33f8c2f87fc3e5c9
Roman <@>
2023-08-30 17:34:32 +0400

    feat: add task card

--- src/app/board-task/board-task.component.ts
+++ src/app/board-task/board-task.component.ts
@@ -1,10 +1,31 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-board-task',
   templateUrl: './board-task.component.html',
-  styleUrls: ['./board-task.component.scss']
+  styleUrls: ['./board-task.component.scss'],
 })
 export class BoardTaskComponent {
+  
+  selectedPriority: string = 'low';
 
+  
+  todo = ['Get to work'];
+
+  done = ['Get up', 'Brush teeth'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
 }
