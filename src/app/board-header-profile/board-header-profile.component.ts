7d91a7e95bbfbec5642d9a01f6759c4c362c8616
Roman <@>
2023-09-21 20:32:12 +0400

    feat: add users services with http
    Now user can change information about yourself and delete account  Also add new confirmation window

--- src/app/board-header-profile/board-header-profile.component.ts
+++ src/app/board-header-profile/board-header-profile.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, catchError } from 'rxjs';
 import { ProfileChangeDialogComponent } from '../profile-change-dialog/profile-change-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
-import { SlicePipe } from '@angular/common';
 import { FakeAuthService } from '../fake-auth.service';
 
 @Component({
@@ -13,20 +12,54 @@ import { FakeAuthService } from '../fake-auth.service';
 export class BoardHeaderProfileComponent implements OnInit {
   constructor(public dialog: MatDialog, private authService: FakeAuthService) {}
 
-  name: string | null = null;
-  location?: string = 'Tbilisi, Georgia';
+  name: string | null = '';
+  // location?: string = 'Tbilisi, Georgia';
+  login: string | null = '';
+  password: string | null = '';
+  userId: string | null = '';
 
   options: boolean = false;
 
   ngOnInit(): void {
-    this.findName();
+    this.getLogin();
+    this.findUser();
   }
 
-  findName(): void {
-    let name = localStorage.getItem('login');
-    if (name) {
-      this.name = name;
-    }
+  findUser(): void {
+    this.authService.getUserAll().subscribe(
+      (users) => {
+        const user = users.find((user: any) => this.login === user.login);
+        if (user) {
+          this.userId = user._id;
+          this.name = user.name;
+
+          // Используйте отладочный метод для вывода информации
+          this.debugInfo('User found:', user);
+        } else {
+          // Если пользователь не найден, также выведите информацию
+          this.debugInfo('User not found for login:', this.login);
+        }
+      },
+      (error) => {
+        // Обработка ошибок и вывод информации
+        this.debugInfo('Error occurred:', error);
+      }
+    );
+  }
+
+  debugInfo(message: string, data: any): void {
+    console.log(message, data);
+  }
+
+  // findName(): void {
+  //   let name = localStorage.getItem('login');
+  //   if (name) {
+  //     this.name = name;
+  //   }
+  // }
+
+  getLogin() {
+    this.login = this.authService.getLogin();
   }
 
   showOptions() {
@@ -40,7 +73,8 @@ export class BoardHeaderProfileComponent implements OnInit {
   showChangeDialog(): Observable<profileData> {
     const profileData: profileData = {
       name: this.name,
-      location: this.location,
+      login: this.login,
+      password: this.password,
     };
     const dialogRef = this.dialog.open(ProfileChangeDialogComponent, {
       width: '400px',
@@ -52,19 +86,48 @@ export class BoardHeaderProfileComponent implements OnInit {
   updateProfile() {
     this.showChangeDialog().subscribe((result) => {
       if (result) {
-        
-        this.name = result.name;
-        this.location = result.location;
+        console.log(result);
+        // this.login = result.login;
+        // this.name = result.name;
+        this.login = 'Updating profile...';
+
+        this.authService.putUserById(this.userId!, result).subscribe(
+          (response) => {
+            // Обработка успешного ответа от сервера, если это необходимо.
+            console.log('Профиль обновлен успешно!', response);
+
+            this.login = result.login;
+            this.name = result.name;
+            localStorage.setItem('login', this.login!)
+            // this.location = result.location;
+          },
+          (error) => {
+            // Обработка ошибки при отправке запроса на сервер.
+            console.error('Произошла ошибка при обновлении профиля:', error);
+          }
+        );
       }
     });
   }
 
+  deleteProfile() {
+    this.authService.deleteUserById(this.userId!).subscribe(
+      (response) => {
+        console.log('Профиль успешно удален!', response);
+      },
+      (error) => {
+        console.error('Произошла ошибка при удалении профиля:', error);
+      }
+    );
+  }
+
   logout() {
-    this.authService.logout()
+    this.authService.logout();
   }
 }
 
 export interface profileData {
   name: string | null;
-  location?: string;
+  login: string | null;
+  password: string | null;
 }
--- src/app/fake-auth.service.ts
+++ src/app/fake-auth.service.ts
@@ -6,9 +6,11 @@ import {
   ErrorDialogData,
   WarningDialogComponent,
 } from './warning-dialog/warning-dialog.component';
-import { HttpClient } from '@angular/common/http';
-import { Observable, map } from 'rxjs';
-import { User } from './sign-up/sign-up.component';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, catchError, map, of, switchMap, tap } from 'rxjs';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { profileData } from './board-header-profile/board-header-profile.component';
+import { ConfirmationDialogComponent, ConfirmationDialogData } from './confirmation-dialog/confirmation-dialog.component';
 
 @Injectable({
   providedIn: 'root',
@@ -17,6 +19,11 @@ export class FakeAuthService {
   private isAuthenticated = false;
 
   private apiUrl = 'https://sheveleffroman-final-task-backend.onrender.com';
+
+  private tokenKey: string | null = null;
+  public username: string | null = null;
+  public userId: string | null = null;
+  jwtHelperService = new JwtHelperService();
 
   private loggedInUser: string | null = null;
 
@@ -35,6 +42,20 @@ export class FakeAuthService {
         callback(); // Вызываем колбэк-функцию только после закрытия окна
       }
     });
+  }
+
+  showConfirmationDialog(title: string, message: string): Observable<boolean> {
+    const confirmationDialogData: ConfirmationDialogData = {title, message, deleteProfile: true};
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      width:'400px',
+      data: confirmationDialogData,
+    })
+
+    return dialogRef.afterClosed().pipe(
+      tap((result) => {
+        console.log('Диалоговое окно закрыто, результат:', result);
+      })
+    );
   }
   
 
@@ -56,54 +77,105 @@ export class FakeAuthService {
     localStorage.setItem('token', token);
   }
 
-  // login(login: string, password: string): Observable<boolean> {
-  //   // Создайте объект данных для аутентификации
-  //   const authenticationData = { login, password };
+  getToken() {
+    return localStorage.getItem('token');
+  }
 
-  //   // Отправьте POST-запрос на сервер для аутентификации
-  //   return this.http.post<any>(`${this.apiUrl}/auth/signin`, authenticationData)
-  //     .pipe(
-  //       map(response => {
-  //         // Если сервер вернул успешный ответ, устанавливаем флаг аутентификации
-  //         // и сохраняем информацию об аутентификации в локальном хранилище
-  //         if (response) {
-  //           this.isAuthenticated = true;
-            // localStorage.setItem('isAuth', 'true');
-  //           return true;
-  //         } else {
-  //           // Если сервер вернул ошибку, обрабатываем её здесь
-  //           // Показываем модальное окно с сообщением об ошибке
-  //           this.showErrorDialog(
-              // 'Ошибка входа',
-              // 'Неправильные учетные данные. Пожалуйста, проверьте ваше имя пользователя и пароль.'
-  //           );
-  //           return false;
-  //         }
-  //       })
-  //     );
+  removeToken() {
+    localStorage.removeItem('login');
+    localStorage.removeItem('token');
+  }
+
+  getLogin() {
+    return localStorage.getItem('login');
+  }
+
+  // isLoggedIn() {
+  //   return this.getToken() !== null;
   // }
 
+  authorize() {
+    if (!this.jwtHelperService.isTokenExpired(localStorage.getItem('token'))) {
+      this.tokenKey = localStorage.getItem('token') as string;
+      this.username = localStorage.getItem('token') as string;
+      this.router.navigate(['/welcome']);
+    } else {
+      localStorage.removeItem('login');
+      localStorage.removeItem('token');
+    }
+  }
 
+  getUserAll(): Observable<any> {
+    this.authorize();
+    const headers = new HttpHeaders({
+      accept: 'application/json',
+      Authorization: `Bearer ${this.tokenKey}`,
+    });
 
-  // login(username: string, password: string): boolean {
-  //   // Здесь вы можете сравнивать данные с какими-то предопределенными значениями
-  //   if (username === 'roman' && password === 'Romanik16)') {
-  //     this.isAuthenticated = true;
-  //     localStorage.setItem('isAuth', 'true');
-  //     return true;
-  //   } else {
-  //     // Показать универсальное модальное окно с предупреждением об ошибке
-  //     this.showErrorDialog(
-  //       'Ошибка входа',
-  //       'Неправильные учетные данные. Пожалуйста, проверьте ваше имя пользователя и пароль.'
-  //     );
-  //     return false;
-  //   }
-  // }
+    const requestOptions = { headers: headers };
+
+    return this.http
+      .get<any>(`${this.apiUrl}/users`, requestOptions)
+  }
+
+  getUserById(id: string): Observable<any> {
+    this.authorize();
+    const headers = new HttpHeaders({
+      accept: 'application/json',
+      Authorization: `Bearer ${this.tokenKey}`,
+    });
+
+    const requestOptions = { headers: headers };
+
+    return this.http
+      .get<any>(`${this.apiUrl}/users/${id}`, requestOptions)
+  }
+
+  putUserById(uderId: string, userData: profileData): Observable<any> {
+    this.authorize();
+    const headers = new HttpHeaders({
+      accept: 'application/json',
+      Authorization: `Bearer ${this.tokenKey}`,
+    });
+
+    const requestOptions = { headers: headers };
+
+    return this.http
+      .put<any>(`${this.apiUrl}/users/${uderId}`, userData, requestOptions)
+  }
+
+  deleteUserById(userId: string): Observable<any> {
+    this.authorize();
+    const headers = new HttpHeaders({
+      accept: 'application/json',
+      Authorization: `Bearer ${this.tokenKey}`,
+    });
+  
+    const requestOptions = { headers: headers };
+  
+    return this.showConfirmationDialog('Вы точно хотите удалить аккаунт?', 'После нажатия на кнопку все данные будут стёрты и вы будете перенаправлены на главную страницу').pipe(
+      switchMap((result: boolean) => {
+        if (result) {
+          // Если пользователь подтвердил удаление
+          this.isAuthenticated = false;
+          localStorage.setItem('isAuth', 'false');
+          this.removeToken();
+          localStorage.removeItem('loggedInUser');
+          this.router.navigate(['/welcome']);
+          return this.http.delete<any>(`${this.apiUrl}/users/${userId}`, requestOptions);
+        } else {
+          // If user did not confirm, return an empty observable
+          return of(null);
+        }
+      })
+    );
+  }
+  
 
   logout(): void {
     this.isAuthenticated = false;
     localStorage.setItem('isAuth', 'false');
+    this.removeToken();
     localStorage.removeItem('loggedInUser');
     this.showErrorDialog(
       'Вы вышли из аккаунта',
