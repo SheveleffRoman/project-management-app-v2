a223cf89333acb8f05d322d350cddf246b6b43db
Roman <@>
2023-10-20 14:15:22 +0400

    fix: update tests

--- src/app/task.service.spec.ts
+++ src/app/task.service.spec.ts
@@ -1,4 +1,7 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing'; // Import HttpClientTestingModule
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar'; // Import MatSnackBar and MatSnackBarModule
 
 import { TaskService } from './task.service';
 
@@ -6,7 +9,14 @@ describe('TaskService', () => {
   let service: TaskService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [MatDialogModule, HttpClientTestingModule, MatSnackBarModule], // Import MatDialogModule and HttpClientTestingModule
+      providers: [
+        // Provide a mock implementation for MatDialog
+        { provide: MatDialog, useValue: { open: () => {} } },
+        { provide: MatSnackBar, useValue: { open: () => {} } },
+      ]
+    });
     service = TestBed.inject(TaskService);
   });
 
